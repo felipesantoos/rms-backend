3dacad82896675068a47ebac8b1ec83df602eda5
Felipe da Silva Santos <felipedssantos811@gmail.com>
2023-10-19 13:10:58 -0300

    Add list projects route (#3)

--- src/core/services/ProjectServices.go
+++ src/core/services/ProjectServices.go
@@ -4,7 +4,7 @@ import (
 	"github.com/google/uuid"
 	"rms-backend/src/core/domain/errors"
 	"rms-backend/src/core/domain/project"
-	primary "rms-backend/src/core/interfaces/primary"
+	"rms-backend/src/core/interfaces/primary"
 	"rms-backend/src/core/interfaces/repository"
 )
 
@@ -18,4 +18,20 @@ func NewProjectServices(projectRepository repository.IProjectRepository) primary
 
 func (this *projectServices) Create(projectObject project.Project) (*uuid.UUID, errors.Error) {
 	return this.projectRepository.Create(projectObject)
+}
+
+func (this *projectServices) List() ([]project.Project, errors.Error) {
+	return this.projectRepository.List()
+}
+
+func (this *projectServices) Get(id uuid.UUID) (project.Project, errors.Error) {
+	return nil, nil
+}
+
+func (this *projectServices) Update(projectObject project.Project) errors.Error {
+	return nil
+}
+
+func (this *projectServices) Delete(id uuid.UUID) errors.Error {
+	return nil
 }
---
+++ src/ui/api/handlers/dto/response/project.go
@@ -0,0 +1,45 @@
+package response
+
+import (
+	"github.com/google/uuid"
+	"rms-backend/src/core/domain/project"
+	"time"
+)
+
+type Project struct {
+	ID          uuid.UUID  `json:"id"`
+	Name        string     `json:"name"`
+	Alias       string     `json:"alias"`
+	Description string     `json:"description"`
+	IsActive    bool       `json:"is_active"`
+	CreatedAt   time.Time  `json:"created_at"`
+	UpdatedAt   time.Time  `json:"updated_at"`
+	DeletedAt   *time.Time `json:"deleted_at"`
+}
+
+type projectBuilder struct{}
+
+func ProjectBuilder() *projectBuilder {
+	return &projectBuilder{}
+}
+
+func (*projectBuilder) BuildFromDomain(data project.Project) Project {
+	return Project{
+		ID:          data.ID(),
+		Name:        data.Name(),
+		Alias:       data.Alias(),
+		Description: data.Description(),
+		IsActive:    data.IsActive(),
+		CreatedAt:   data.CreatedAt(),
+		UpdatedAt:   data.UpdatedAt(),
+		DeletedAt:   data.DeletedAt(),
+	}
+}
+
+func (builder *projectBuilder) BuildFromDomainList(list []project.Project) []Project {
+	result := []Project{}
+	for _, item := range list {
+		result = append(result, builder.BuildFromDomain(item))
+	}
+	return result
+}
